bf5a7eebc5880ff32974d9a239b7d8b3e9408703
shika900 <132929430+shika900@users.noreply.github.com>
2025-06-10 18:24:12 +0300

    Add files via upload

---
+++ NEW/payload.js
@@ -0,0 +1,49 @@
+function load_gtabeef138(){
+LoadedMSG="Installer gta5 rdr2 prx Loaded !!!";
+LoadFromGHBLS('installergta5rdr2prx.bin');
+}
+
+function load_otoolbox(){
+LoadedMSG="Orbis-Toolbox Loaded !!!";
+LoadFromGHBLS('OrbisToolbox.bin');
+}
+
+function load_dumper(){
+LoadedMSG="Dumper v2 Loaded !!!";
+LoadFromGHBLS('gamedumper.bin');
+}
+
+function load_webrte(){
+LoadedMSG="WebRte Loaded !!!";
+LoadFromGHBLS('webrte.bin');
+}
+
+function load_ps4debug(){
+LoadedMSG="PS4Debug v1.0.15  Loaded !!!";
+LoadFromGHBLS('ps4debug.bin');
+}
+
+function load_cacheinstall(){
+LoadedMSG="restore DB Loaded !!!";
+LoadFromGHBLS('restore.bin');
+}
+
+function load_cacheinstall2(){
+LoadedMSG="Backup DB Loaded !!!";
+LoadFromGHBLS('backup.bin');
+}
+
+function load_pkg(){
+LoadedMSG="Backup Pkg v1.1 Loaded!!! Load your game!!!";
+LoadFromGHBLS('pkgbackup.bin');
+}
+
+function load_linux4gb(){
+LoadedMSG="Linux 4G Pro Loaded!!!";
+LoadFromGHBLS('linux4gbPhat.bin');
+}
+
+function load_linux4gbp(){
+LoadedMSG="Linux 4G Pro Loaded!!!";
+LoadFromGHBLS('linux4gbpro.bin');
+}
